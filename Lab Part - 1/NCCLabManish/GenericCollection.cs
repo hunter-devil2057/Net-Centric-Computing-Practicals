650b6720d42a20b223e6d4d45cf35ed856e8241a
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:01:34 +0545

    Generic Collection in C#

---
+++ Lab Part - 1/NCCLabManish/GenericCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class GenericCollection
+    {
+        static void Main(string[] args)
+        {
+            // Create a generic List collection of integers
+            List<int> numbers = new List<int>();
+
+            // Add elements to the collection
+            numbers.Add(69);
+            numbers.Add(79);
+            numbers.Add(89);
+
+            // Display elements in the collection
+            Console.WriteLine("Elements in the List:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
