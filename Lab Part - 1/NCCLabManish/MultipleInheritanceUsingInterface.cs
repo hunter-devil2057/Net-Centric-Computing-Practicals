ec2e370ad9da757cd60790b3eddfdea81884b207
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:58:35 +0545

    Multiple Inheritance Using Interface in C#

---
+++ Lab Part - 1/NCCLabManish/MultipleInheritanceUsingInterface.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class MultipleInheritanceUsingInterface
+    {
+        // Interface for Shape
+        public interface IShape
+        {
+            double Area();
+        }
+
+        // Interface for Color
+        public interface IColor
+        {
+            string GetColor();
+        }
+
+        // Concrete class implementing IShape and IColor
+        public class Rectangle : IShape, IColor
+        {
+            private double length;
+            private double width;
+            private string color;
+
+            public Rectangle(double l, double w, string c)
+            {
+                length = l;
+                width = w;
+                color = c;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+
+            public string GetColor()
+            {
+                return color;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Create an object of the Rectangle class
+            Rectangle rectangle = new Rectangle(15, 9, "Red");
+
+            // Call methods from both interfaces
+            double area = rectangle.Area();
+            string color = rectangle.GetColor();
+
+            // Output the results
+            Console.WriteLine("Area of the rectangle: " + area);
+            Console.WriteLine("Color of the rectangle: " + color);
+        }
+    }
+}
