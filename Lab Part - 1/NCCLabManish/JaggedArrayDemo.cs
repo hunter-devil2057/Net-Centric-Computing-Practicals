6e5f79dfc1505a934cb30aca11b7593e1d5686e7
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:03:36 +0545

    Jagged Array in C#

---
+++ Lab Part - 1/NCCLabManish/JaggedArrayDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    class JaggedArrayDemo
+    {
+        static void Main(string[] args)
+        {
+            // Define a jagged array of strings to store grocery items
+            string[][] groceryItems =
+            {
+                new string[] { "Apples", "Bananas", "Oranges" },        // Fruits aisle
+                new string[] { "Bread", "Milk", "Eggs", "Butter" },     // Dairy aisle
+                new string[] { "Rice", "Pasta", "Flour", "Beans" },     // Grains aisle
+                new string[] { "Chicken", "Beef", "Fish" }              // Meat aisle
+            };
+
+            // Display the grocery items aisle by aisle
+            for (int list = 0; list < groceryItems.Length; list++)
+            {
+                Console.WriteLine($"List No. {list + 1}:");
+                foreach (var item in groceryItems[list])
+                {
+                    Console.WriteLine($"- {item}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
