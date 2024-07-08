4a17376349961d59e5c09a89e73442bfaf4b5a3f
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:47:54 +0545

    Anonymous Method And Events in C#

---
+++ Lab Part - 1/NCCLabManish/AnonymousMethodAndEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class AnonymousMethodAndEvent
+    {
+        // Define a delegate for the event
+        public delegate void EventHandler(string message);
+
+        // Define an event of type EventHandler
+        public static event EventHandler MyEvent;
+
+        static void Main(string[] args)
+        {
+            // Anonymous method assigned to the event
+            MyEvent += delegate (string message)
+            {
+                Console.WriteLine("Anonymous Method: " + message);
+            };
+
+            // Fire the event
+            MyEvent?.Invoke("Event fired with anonymous method");
+        }
+    }
+}
