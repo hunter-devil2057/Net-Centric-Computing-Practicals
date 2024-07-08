1c0a18c8458dfdd57aa7ccf335a9ad9492af1af4
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:05:33 +0545

    LINQ with Lambda Expression in C#

---
+++ Lab Part - 1/NCCLabManish/LINQWithLambdaExpression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    public class LINQWithLambda
+    {
+        public static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
+            // Lambda Expression inside LINQ 'Where()' method used as condition
+            List<int> res = nums.Where(x => x > 2).ToList();
+            foreach (int i in res)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
