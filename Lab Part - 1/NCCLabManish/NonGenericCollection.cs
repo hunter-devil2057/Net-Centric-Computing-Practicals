eea82a0d790ac84efa5fb7965a64afad6135bfbe
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:01:18 +0545

    Non-Generic Collection in C#

---
+++ Lab Part - 1/NCCLabManish/NonGenericCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class NonGenericCollection
+    {
+        static void Main(string[] args)
+        {
+            // Create a non-generic ArrayList collection
+            ArrayList list = new ArrayList();
+
+            // Add elements to the collection
+            list.Add(69);
+            list.Add("Good Morning. Have a great day.");
+            list.Add(3.14);
+
+            // Display elements in the collection
+            Console.WriteLine("Elements in the ArrayList:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
