e039e5ff9a1d02304790bb012136fcd42085d070
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:07:24 +0545

    Method Overriding in C#

---
+++ Lab Part - 1/NCCLabManish/MethodOverriding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    class ParentClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Superclass Display Method.");
+        }
+    }
+    class ChildClass: ParentClass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Subclass Display Method.");
+        }
+    }
+    internal class MethodOverriding
+    {
+        static void Main(string[] args)
+        {
+            ParentClass obj1 = new ChildClass();
+            obj1.Display();
+        }
+    }
+}
