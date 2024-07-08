c2ad0038b36b0ebced17a0b625d9812c752e5fa3
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:50:36 +0545

    Use of base Keyword to call Base Class Methods in C#

---
+++ Lab Part - 1/NCCLabManish/BaseClassFieldDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    public class Animal
+    {
+        public string color = "white";
+    }
+    public class Dog: Animal
+    {
+        string color = "black";
+        public void DisplayColor()
+        {
+            Console.WriteLine(base.color);
+            Console.WriteLine(color);
+        }
+    }
+    public class BaseClassFieldDemo
+    {
+        public static void Main(string[] args)
+        {
+            Dog d = new Dog();
+            d.DisplayColor();
+        }
+    }
+}
