d08cd84510e1c2f6dababe422c039c600f0f6db1
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:54:38 +0545

    Delegate in C#

---
+++ Lab Part - 1/NCCLabManish/Delegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    // Delegate declaration
+    public delegate void MyDelegate(string message);
+
+    // Class to demonstrate delegates
+    public class Delegate
+    {
+        // Method to be assigned to the delegate
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine("Message from the delegate: " + message);
+        }
+
+        // Main method
+        public static void Main(string[] args)
+        {
+            // Create an instance of the delegate
+            MyDelegate delegateInstance = new MyDelegate(DisplayMessage);
+
+            // Call the delegate
+            delegateInstance("Hello, World!");
+        }
+    }
+}
