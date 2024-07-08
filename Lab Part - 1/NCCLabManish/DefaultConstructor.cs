b369b56637b93da6c6c7b2b5897ccb489b8c086a
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:54:11 +0545

    Default Constructor in C#

---
+++ Lab Part - 1/NCCLabManish/DefaultConstructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class DefaultConstructor
+    {
+        int id;
+        string Name;
+        //DefaultConstructor() is a constructor of a class DefaultConstructor
+        public DefaultConstructor()
+        {
+            Console.WriteLine("Hello, This is Default Constructor.");
+            id = 747701;
+            Name = "Manish Shiwakoti";
+        }
+        static void Main(string[] args)
+        {
+            //Creating Object of class Constructor
+            DefaultConstructor demo = new DefaultConstructor();
+            Console.WriteLine("Id: "+demo.id);
+            Console.WriteLine("Name: " + demo.Name);
+        }
+    }
+}
