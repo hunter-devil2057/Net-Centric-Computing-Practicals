fa03b11e2ba9cee20cc327d003e63c7d29817946
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:04:52 +0545

    Static Constructor in C#

---
+++ Lab Part - 1/NCCLabManish/StaticConstructor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    class StaticConstructor
+    {
+        static int count;
+        public StaticConstructor()
+        {
+            Console.WriteLine("Hello from Instance Constructor");
+            count++;
+        }
+        static StaticConstructor()
+        {
+            Console.WriteLine("Hello from Static Constructor");
+            count = 400;
+        }
+        static void Main(string[] args)
+        {
+            StaticConstructor d1 = new StaticConstructor();
+            Console.WriteLine("Hello from main");
+            Console.WriteLine("value of count= " + count);
+            StaticConstructor d2 = new StaticConstructor();
+            Console.WriteLine("value of count= " + count);
+        }
+    }
+}
