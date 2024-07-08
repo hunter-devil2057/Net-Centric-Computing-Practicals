36852d7a6b1a50f3168988fb88106482e553945f
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:48:47 +0545

    Asynchronous Programming in C#

---
+++ Lab Part - 1/NCCLabManish/AsyncProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class AsyncProgram
+    {
+        public async Task<int> GetUrlContentLengthAsync(string url)
+        {
+            Console.WriteLine($"Connecting to {url} and fetching the contents..");
+            var client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync(url);
+            DoIndependentWork();
+            string contents = await getStringTask;
+            Console.WriteLine("Fetching completed !");
+            return contents?.Length ?? 0; // Check if contents is not null before accessing its properties
+        }
+
+        void DoIndependentWork()
+        {
+            Console.WriteLine("Working..");
+            Console.WriteLine("Completed.");
+        }
+
+        static async Task Main(string[] args)
+        {
+            AsyncProgram ap = new AsyncProgram();
+            int result = await ap.GetUrlContentLengthAsync("https://cdcsit.edu.np/");
+            Console.WriteLine("Length of the contents : {0}", result);
+        }
+    }
+}
---
+++ Lab Part - 1/NCCLabManish/AsynchronousProgramming.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace NCCLabManish
+{
+    public class AsynchronousProgramming
+    {
+        public static async Task MethodA()
+        {
+            await Task.Run(() => {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("This is Method A");
+                }
+            });
+        }
+        public static void MethodB()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("This is Method B");
+            }
+        }
+        static void Main(string[] args)
+        {
+            MethodA();
+            MethodB();
+        }
+    }
+}
