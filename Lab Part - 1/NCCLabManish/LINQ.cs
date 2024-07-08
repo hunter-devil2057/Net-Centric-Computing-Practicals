6c33bb63aada682c8c7a1a6b50988a2596c7e62b
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:06:54 +0545

    Language INtegrated Query, LINQ in C#

---
+++ Lab Part - 1/NCCLabManish/LINQ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class LINQ
+    {
+        static void Main(string[] args)
+        {
+            // Sample list of numbers
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // LINQ query to filter even numbers
+            var evenNumbers = from num in numbers
+                              where num % 2 == 0
+                              select num;
+
+            // Displaying even numbers using LINQ query
+            Console.WriteLine("Even numbers using LINQ:");
+            foreach (var num in evenNumbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
