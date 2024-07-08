acb1a9bd92d2974749f16cc661f85197dc2b78cb
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:04:35 +0545

    Taking Input from Keyboard and Writing them to a File

---
+++ Lab Part - 1/NCCLabManish/KeyboardInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NCCLabManish
+{
+    class KeyboardInput
+    {
+        static void Main(string[] args)
+        {
+            // Get the file path from the user
+            Console.WriteLine("Enter the file path:");
+            string filePath = Console.ReadLine();
+
+            // Ask user for input
+            Console.WriteLine("Enter text (type 'exit' to finish):");
+
+            // Open or create the file for writing
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                string input;
+                // Continue taking input until the user types 'exit'
+                while ((input = Console.ReadLine()) != "exit")
+                {
+                    // Write the input to the file
+                    writer.WriteLine(input);
+                }
+            }
+
+            Console.WriteLine("Text has been written to the file.");
+        }
+    }
+}
