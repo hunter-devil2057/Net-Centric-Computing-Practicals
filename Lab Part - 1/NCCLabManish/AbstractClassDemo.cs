0104ca0b555f807dead8bbbaca9fa5074621026d
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 14:46:52 +0545

    Abstract Class in C#

---
+++ Lab Part - 1/NCCLabManish/AbstractClassDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    // Abstract class
+    abstract class Shape
+    {
+        // Abstract method
+        public abstract double Area();
+    }
+
+    // Concrete class implementing the abstract class
+    class Rectangle : Shape
+    {
+        // Fields
+        private double length;
+        private double width;
+
+        // Constructor
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        // Implementing the abstract method from the abstract class
+        public override double Area()
+        {
+            return length * width;
+        }
+    }
+
+    // Program class with the Main method
+    class AbstractClassDemo
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of the derived class
+            Rectangle rectangle = new Rectangle(15, 7);
+
+            // Call the method
+            double area = rectangle.Area();
+            Console.WriteLine("Area of the rectangle: " + area);
+        }
+    }
+}
