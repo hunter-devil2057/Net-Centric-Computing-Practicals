f7d31621c9c0ef1333c393060803915e54e5a464
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:02:44 +0545

    Indexer in C#, when Index is of string type

---
+++ Lab Part - 1/NCCLabManish/IndexerTypeString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    internal class IndexerTypeString
+    {
+        private string[] data = new string[5];
+
+        // Indexer when index is of string type
+        public string this[string index]
+        {
+            get { return data[Convert.ToInt32(index)]; }
+            set { data[Convert.ToInt32(index)] = value; }
+        }
+
+        static void Main(string[] args)
+        {
+            IndexerTypeString demo = new IndexerTypeString();
+
+            // Using indexers with string type index
+            demo["0"] = "Rimuru";
+            demo["1"] = "Baji";
+            demo["2"] = "Condoriano";
+
+            // Displaying values
+            Console.WriteLine("Values with string index:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Index {i}: {demo[i.ToString()]}");
+            }
+        }
+    }
+}
