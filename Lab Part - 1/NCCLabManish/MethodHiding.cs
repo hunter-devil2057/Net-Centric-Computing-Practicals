8f4e11b06688d501b9e102ac1630a93737bf552f
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:07:10 +0545

    Method Hiding in C#

---
+++ Lab Part - 1/NCCLabManish/MethodHiding.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    class SuperClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Super Class Display Method.");
+        }
+    }
+    class SubClass: SuperClass
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Sub Class Display Method.");
+        }
+    }
+    internal class MethodHiding
+    {
+        static void Main(string[] args)
+        {
+            SubClass obj = new SubClass();
+            obj.Display();
+        }
+    }
+}
