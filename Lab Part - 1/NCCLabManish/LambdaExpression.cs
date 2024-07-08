4e19ba04e360bf85addfe3616ebcb9cdcfc29a4c
Sheewakoti Manish <manishshiwakoti42@gmail.com>
2024-07-08 15:05:05 +0545

    Lambda Expression in C#

---
+++ Lab Part - 1/NCCLabManish/LambdaExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCCLabManish
+{
+    public delegate double MyDel(double value);
+    public class LambdaExpression
+    {
+        public static void Main(string[] args)
+        {
+            double num = 5.6d;
+            // Lambda Expression
+            MyDel md2 = (x) =>
+            {
+                Console.Write("Square Value is: ");
+                return x * x;
+            };
+            double square = md2(num);
+            Console.WriteLine(square);
+        }
+    }
+}
